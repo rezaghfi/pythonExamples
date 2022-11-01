847554de01f299b4bfc6f4c4c132c45b3e6c3c81
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-11-01 12:31:31 +0330

    create course folder

---
+++ 5_course/jalase2.py
@@ -0,0 +1,146 @@
+#
+# -------------------------------------------------- jalase 2 ---------------------------------------------------
+if 5 <2 :
+  print("5 is smaller than 2")
+
+# it if for this course
+if 5 > 2:
+      print("5 is bigger than 2")
+      x = 1
+x = 1
+y = 3
+z = "hdf"
+"""
+this 
+is 
+comment
+"'"
+"""
+x = 5
+y = "john"
+print(x)
+print(y)
+x = 4
+x = "sally"
+print(x)
+
+# casting
+x = str(3)  # x is '3'
+y = int(3)  # y is 3
+z = float(3) # z is 3.0
+
+print(type(x))
+print(type(y))
+
+myvar = 12
+my_var = 12
+#my var = 12
+#$myvar = 12
+#2var = 12
+
+x = y = z = 12
+x , y , z = "python ", "is ", "awesome"
+print(x)
+print(y)
+print(z)
+print(x, y, z)
+print(x+y+z)
+a = b = c = 12
+print(a+b+c)
+print(a, x)
+
+# global and local variable
+x = 1
+
+def func():
+  x = 2
+  print("local x: ", x)
+
+func()
+print("global x:", x)
+
+# data type
+x = "hello world"
+x = -209999999999999999999999999999999999999999999999999
+x = 20.400000000000000000000000000000000
+x = 1j
+x = ["apple", "banana", "cherry"] # x is list ordered, changed , duplicated
+x = [1,1,1,1]
+x = [1 , 2 , 3 , "hasan"] #
+x = ("apple", "banana", "cherry") # x is tuple
+x = range(6)
+x = {"name":"reza", "age":27} # x is dictionary
+x = {"apple", "banana", "cherry"} # x is set
+x = True # boolean
+x = b"Hello"
+x = bytearray(bytes(2))
+x = memoryview(bytes(3))
+x = None # noneType
+
+# کد هایی که در این قسمت هستند در کلاس حضوری گفته شدند.
+indentation
+# this sample for learning
+"""
+this
+sample
+"""
+x = 1
+y = 2
+print(x)
+print(y)
+z = x + y
+print(z)
+if(z>0):
+    print("z is bigger than zero")
+a121223344 = 12
+a = 12
+A = 11
+age = 1
+Age = 23
+x = 1
+x = "reza"
+print(x)
+x = 1
+x = 1.2     # this is float and size is 32 bit
+x = 1.32434343434
+x = "string1"
+x = False
+x = True
+x = int(1)
+y = str("str1")
+x = "er"
+x = 100
+y = 3.3
+z = x + y
+z = x - y
+z = x * y
+z = x / y
+z = x % y
+z = x ** y
+z += 1  # z = z + 1
+z -= 1 # z = z - 1
+z *= 2 # z = z * 2
+z /= 4 # z = z / 4
+str1 = "str1"
+str2 = "str2"
+str3 = str1 + " " + str2
+print(z)
+print(str3)
+
+list = [1,34,656,34]
+list[0] = 23 # change list item
+print(list[0]) # access list item
+a , b , c ,d = list
+list2 = [1 , "reza" , "hasan", True]
+print(list2[3])
+print(c)
+# practice 1
+# set , dictionary , tuple
+
+in1 = 12
+in2 = 2
+print("sum 12 + 2: "+in1+in2)
+print("sum 12 - 2: "+in1-in2)
+print("sum 12 * 2: "+in1*in2)
+print("sum 12 / 2: "+in1/in2)
+
