384c672830ccc75cd46ba47c81bac9835254b573
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-11-04 18:45:20 +0330

    change project

--- 1_basics/syntax.py
+++ 1_basics/syntax.py
@@ -178,3 +178,117 @@ print(type(z))
 # Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
 #Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
 #Complex numbers are written with a "j" as the imaginary part:
+
+print("-----------------python casting----------------")
+# int() float() str()
+x = int(3.44) # x will be 3
+y = str(3.3434) # Y  will be '3.3434'
+print("-----------------python string----------------")
+print('Hello')
+print("Hello")
+print("""for multiple line""")
+print('''for multiple line''')
+a = "Hello, World"
+print(a[1])
+for x in "banana":
+  print(x)
+print(len(a))
+txt = "The best things in life are free!"
+# in
+print("free" in txt)
+if "free" in txt:
+  print("Yes, 'free' is present.")
+# not in
+print("expensive" not in txt)
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+h = "Hello, World"
+print(h[2:5])
+print(h[:5])
+print(h[-5:-2])
+print("-----------------python Modify String----------------")
+print("h in uppercase is: ",h.upper())
+print("h in lowercase is: ",h.lower())
+b = "    Hello, World      "
+print("strip remove spave from begin and end")
+print(b.strip())
+print("replace character h with g")
+print(a.replace('H','G'))
+print(a.split(","))
+# concatenate strings
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+print("-----------------python Format String----------------")
+age = 25
+txt = "My name is John, I am {}"
+print(txt.format(age))
+q = 2
+i = 368
+m = 34.6
+myOrder = "I want {} pieces of item {} for {} dollars."
+print(myOrder.format(q, i, m))
+#print Escape characters
+txt = "\twe\n \'are the  \r \\ \"mens\" "
+print(txt)
+# there are many method is exist for strings in https://www.w3schools.com/python/python_strings_methods.asp site
+print("-----------------python Booleans----------------")
+print(10 > 5)
+print(10 == 6)
+print(39 < 34)
+print(bool(12))
+print(bool(0), bool(False), bool(None), bool(""), bool(0), bool(()), bool([]), bool({}))
+# function can return boolean
+def myFunc():
+  return True;
+if myFunc():
+  print("YES!")
+else:
+  print("NO!")
+
+print("-----------------python Operators----------------")
+print("1.Arithmetic +-*/ ** //", " 2.Assignment = += -= ", " 3.Comparison < > <= >= == !=", " 4.Logical AND OR NOT", " 5.Identity IS , NOT IS", " 6.Membership in , not in", " 7.Bitwise Operators | &")
+
+print("-----------------python Collections----------------")
+a = """Python Collections (Arrays)
+There are four collection data types in the Python programming language:
+
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members."""
+print(a)
+print("-----------------List----------------")
+print("List items are ordered, changeable, and allow duplicate values.")
+thisList = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thisList)
+print(len(thisList))
+
+# a list can contain diffrent data types.
+list1 = ["avd", 34, True, 40, "male"]
+print(type(list1))
+# create list with list constructor
+thisList = list(("apple", 3, 34))
+print(thisList[1]) # Access list items
+print(thisList[:3])
+print(thisList[-1])
+if "apple" in thisList:
+  print("Yes, 'apple' is in the fruits list")
+thisList[1] = "orange" # change item
+thisList[1:3] = ['melon']
+print(thisList)
+thisList.insert(2,"watermelon") # insert new item
+thisList.append("orange") # add item to end of list
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+thislist.remove("banana") # remove specified Item
+thisList.pop(1) # remove specofied index.
+thislist.clear() # clear list
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+[print(x) for x in thislist]
