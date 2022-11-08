84ff6be2d7fc1b5e0bcae5ae7293947c141894c0
Reza Gholamalitabar <reza.gh.fi@gmail.com>
2022-11-08 16:54:33 +0330

    home jalase6

--- 1_basics/function.py
+++ 1_basics/function.py
@@ -26,70 +26,29 @@ drawRectangle(5)
 
 print("create a fuction - calling function - parameters or arguments - number of argument - arbitary arguments by * - keyword argument - arbitory keyword arguments - defualt paarameter value - passing list as a argument - return values - the pass statement - recursion - ")
 
-# number of argument
-def my_function(fName, lName):
-  print(fName + " "+ lName)
+list = ["apple", "cherry","banana"]
+def guess(list):
+  print("choose name from list:", list)
+  for x in list:
+    in1 = input("is your word "+ str(x) + "?")
+    if (in1 == "yes"):
+      print("hoooooray! I found the word.")
+      break
+#guess(list)
 
-# my_fuction("email") it have error
-my_function("reza","gholamalitabar")
-
-#arbitrary arguments, *args
-# it is overloaded function
-def my_function(*kids):
-  print("the youngest child is " + kids[2])
-my_function("Email", "Tobias", "Linus")
-
-#keyword arguments
-def my_function(child3, child2, child1):
-  print("The oldest child is "+ child3)
-my_function(child1="Email", child2 = "Tobias", child3= "Linus")
-my_function("Email", "Tobias", "Linus")
-
-#arbitrary keyword arguments, **kwargs
-def my_function(**kids):
-  print("His last name is " + kids["lname"])
-my_function(fname = "Tobias", lname = "Refsnes")
-
-# default parameter
-def func(country = "iran"):
-  print("i am from " + country)
-func()
-func("india")
-# passing a list as an argument
-def func1(food):
-  for x in food:
-    print(x)
-fruits = ["apple", "banana", "cherry"]
-func1(fruits)
-
-#return values
-def func2(x):
-  return 4 * x
-print(func2(2))
-
-#pass statement
-def func3():
-  pass
-
-#Recursion
-def tri_recursion(k):
-  if (k > 0):
-    result = k + tri_recursion(k - 1)
+def max():
+  a = int(input("enter input1:"))
+  b = int(input("enter input2:"))
+  c = int(input("enter input3:"))
+  if(a > b):
+    if (a > c):
+      return a
+    else:
+      return c
   else:
-    result = 0
-  return result
-print(tri_recursion(6))
+    if(b>c):
+      return b
+    else:
+      return c
 
-# python lambda
-# A lambda function can take any number of arguments, but can only have one expression.
-# lambda arguments : expression
-x = lambda a : a + 20
-print(x(5))
-x = lambda a, b : a * b
-print(x(3 ,5))
-
-# The power of lambda is better shown when you use them as an anonymous function inside another function.
-def myfunc(n):
-  return lambda a : a * n
-mydoubler = myfunc(2)
-print(mydoubler(11))
+print(max())
