1b991a82d20b4788db7707f1d8c5f1e094c5c93c
reza gholamalitabar <reza.gh.fi@gmail.com>
2023-09-02 05:03:35 +0330

    jalase 1 mostafa

---
+++ mostafa/jalase1.py
@@ -0,0 +1,25 @@
+# python library
+#variable
+#data type
+x = 1
+f = 1.2
+b = True
+y = 2
+y = "2"
+str = "reza"
+print(x)
+
+# این خط کد توضیحی درباره هر چیزی می تونه باشه که اجرا نمی شود.
+
+# (Phranthises) {curly-brace} [bracket] , -> comma (. dot) (" double cotation)
+
+# function تابع یه سری کدهای از قبل نوشته شده در قالب تابع می باشد
+
+# call function 
+print(y)
+
+tol = float(input("tol khod ra vared kon:"))
+arz = float(input("arz khod ra vared kon:"))
+
+print(tol * arz)
+
